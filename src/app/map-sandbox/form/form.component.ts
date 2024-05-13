59eae9aac7c71a018c0c9885fcbe15fae8a51b18
JoseLeMalin <seb_488@hotmail.fr>
2024-05-13 18:18:44 +0200

    Form professional 2

--- src/app/map-sandbox/form/form.component.ts
+++ src/app/map-sandbox/form/form.component.ts
@@ -2,34 +2,46 @@ import { Component } from "@angular/core";
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ButtonsComponent } from "../../components/buttons/buttons.component";
 import { TabViewModule } from "primeng/tabview";
+import { DividerModule } from "primeng/divider";
 
 @Component({
   selector: "app-form",
   standalone: true,
-  imports: [ReactiveFormsModule, ButtonsComponent, FormsModule, TabViewModule],
+  imports: [ReactiveFormsModule, ButtonsComponent, FormsModule, TabViewModule, DividerModule],
   templateUrl: "./form.component.html",
   styleUrl: "./form.component.css",
 })
 export class FormComponent {
-  nameTest = new FormControl({
+  cityName = new FormControl({
     value: "test SHELL",
     disabled: false,
   });
-  age = new FormControl({
+  population = new FormControl({
     value: -1,
     disabled: false,
   });
+  latitude = new FormControl({
+    value: 0,
+    disabled: false,
+  });
+  longitude = new FormControl({
+    value: 0,
+    disabled: false,
+  });
   formGroup = new FormGroup({
-    nameTest: this.nameTest,
-    age: this.age,
+    cityName: this.cityName,
+    population: this.population,
+    coordinates: new FormGroup({
+      latitude: this.latitude,
+      longitude: this.longitude,
+    }),
   });
   constructor() {}
   functioncall(e: MouseEvent) {
-    console.log("event in the functioncall parent", this.nameTest.value, e);
+    console.log("event in the functioncall parent", this.cityName.value, e);
   }
 
   onSubmit() {
-
     console.log("Form submitted", this.formGroup.value);
   }
 }

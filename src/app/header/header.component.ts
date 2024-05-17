482ef4ad100a516b7df0591dd0a5c19479e37a26
JoseLeMalin <seb_488@hotmail.fr>
2024-05-17 16:33:40 +0200

    Fix buttons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
 import { Router, RouterModule } from "@angular/router";
+import { RouterButtonComponent } from "@components/buttons/router-button/router-button.component";
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent, RouterButtonComponent],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.css",
 })
